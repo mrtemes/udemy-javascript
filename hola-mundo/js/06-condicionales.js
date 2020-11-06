cffaf068b60d89d5a65292d0b2145304214c50d3
Temes <mrtemes@gmail.com>
2020-11-06 22:01:09 +0100

    Finalizada la sección 08.

--- hola-mundo/js/06-condicionales.js
+++ hola-mundo/js/06-condicionales.js
@@ -39,3 +39,33 @@ if (edad >= 18) {
 } else {
   console.log(nombre + " tiene " + edad + " años, es menor de edad.");
 }
+
+
+/*
+Operadores  lógicos.
+AND(Y): &&
+OR(o): ||
+Negación: !
+ */
+
+var year = 2018;
+
+// Negación.
+if (year != 2016) {
+  console.log ("El año no es 2016. Realmente es: " + year);
+
+}
+
+// AND
+if(year != 2016 && year <= 2020 && year != 2018){
+  console.log("Estamos en la era actual.");
+} else {
+  console.log("Estamos en la era postmoderna.");
+}
+
+// OR
+if(year == 2008 || (year >= 2018 && year == 2028)) {
+  console.log("El año acaba en 8.");
+} else {
+  console.log ("AÑO NO REGISTRADO.")
+}
